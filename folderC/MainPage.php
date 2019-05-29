4b3b184e3b5fa024be1801e13c34d2927ea01953
nofar <nofaralfasi@gmail.com>
2019-05-29 21:27:41 +0300

    updated

--- folderC/MainPage.php
+++ folderC/MainPage.php
@@ -10,7 +10,7 @@ $temp = "My site";
 $text = "5 Most Beautiful Places I've Been";
 $fh = @fopen($filename, 'w') or die("can't open file");
 fwrite($fh, '<html>  <body bgcolor=' . $_POST["colorPick"] . '>');
-fwrite($fh, '<header ><img src="../folderD/upload/' . $_FILES["file"]["name"] . '" width="70%" height="250px"></header>');
+fwrite($fh, '<header ><img src="../folderD/upload/' . $_FILES["file"]["name"] . '" width="100%" height="250px"></header>');
 fwrite($fh, '<table  width="70%" border =2 cellpadding="2"> <tr>');
 if ($_POST["aLink"] != "" && $_POST["aLink"] != 2) {
     fwrite($fh, '<td align="center"><h5><a href="a.html">' . $_POST["aLink"] . ' </a> </h5></td>');
@@ -28,7 +28,7 @@ if ($_POST["dLink"] != "" && $_POST["dLink"] != 2) {
 if ($_POST["eLink"] != "" && $_POST["eLink"] != 2) {
     fwrite($fh, '<td align="center"><h5><a href="e.html">' . $_POST["eLink"] . ' </a> </h5></td>');
 }
-fwrite($fh, '</tr><table>');
+fwrite($fh, '</tr></table>');
 fwrite($fh, '</body></html>');
 fclose($fh);
 //$myfile= @fopen("Gallery.html", "r") ordie("Unable to open file!");
