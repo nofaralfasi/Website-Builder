fafcd48c00785eef8745a0d79e452eea8439585e
nofar <nofaralfasi@gmail.com>
2019-06-02 13:07:36 +0300

    pretty

--- folderC/MainPage.php
+++ folderC/MainPage.php
@@ -9,24 +9,27 @@ move_uploaded_file($_FILES["file"]["tmp_name"], "../folderD/upload/" . $_FILES["
 $temp = "My site";
 $text = "5 Most Beautiful Places I've Been";
 $fh = @fopen($filename, 'w') or die("can't open file");
-fwrite($fh, '<html>  <body bgcolor=' . $_POST["colorPick"] . '><center>');
-fwrite($fh, '<header ><img class="main_m" src="../folderD/upload/' . $_FILES["file"]["name"] . '"></header>');
+
+@fwrite($fh, '<html lang="en"> <head><title>' . $_FILES["file"]["name"] . '</title><link href="style.css" rel="stylesheet"></head>');
+@fwrite($fh, '<body><script>$("body").css("background-color", "' . $_POST["colorPick"] . '");</script>');
+@fwrite($fh, '<center><header><img class="main_m" src="upload/' . $_FILES["file"]["name"] . '"></header></center> ');
+
 fwrite($fh, '<table> <tr>');
 if ($_POST["aLink"] != "" && $_POST["aLink"] != 2) {
-    fwrite($fh, '<td align="center"><h5><a href="a.html">' . $_POST["aLink"] . ' </a> </h5></td>');
+    fwrite($fh, '<td align="center"><h2><a href="a.html">' . $_POST["aLink"] . ' </a> </h2></td>');
 }
 if ($_POST["bLink"] != "" && $_POST["bLink"] != 2) {
-    fwrite($fh, '<td align="center"><h5><a href="b.html">' . $_POST["bLink"] . ' </a> </h5></td>');
+    fwrite($fh, '<td align="center"><h2><a href="b.html">' . $_POST["bLink"] . ' </a> </h2></td>');
 }
 if ($_POST["cLink"] != "" && $_POST["cLink"] != 2) {
 
-    fwrite($fh, '<td align="center"><h5><a href="c.html">' . $_POST["cLink"] . ' </a> </h5></td>');
+    fwrite($fh, '<td align="center"><h2><a href="c.html">' . $_POST["cLink"] . ' </a> </h2></td>');
 }
 if ($_POST["dLink"] != "" && $_POST["dLink"] != 2) {
-    fwrite($fh, '<td align="center"><h5><a href="d.html">' . $_POST["dLink"] . ' </a> </h5></td>');
+    fwrite($fh, '<td align="center"><h2><a href="d.html">' . $_POST["dLink"] . ' </a> </h2></td>');
 }
 if ($_POST["eLink"] != "" && $_POST["eLink"] != 2) {
-    fwrite($fh, '<td align="center"><h5><a href="e.html">' . $_POST["eLink"] . ' </a> </h5></td>');
+    fwrite($fh, '<td align="center"><h2><a href="e.html">' . $_POST["eLink"] . ' </a> </h2></td>');
 }
 fwrite($fh, '</tr></table>');
 fwrite($fh, '</center></body></html>');
