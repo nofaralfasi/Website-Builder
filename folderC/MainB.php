0b47e59406bcd3436a62af28b5fb5a9910ab6185
nofar <nofaralfasi@gmail.com>
2019-06-02 18:05:29 +0300

    update

--- folderC/MainB.php
+++ folderC/MainB.php
@@ -21,34 +21,34 @@ if (isset($_FILES["filekk"]["tmp_name"])) {
 }
 
 $fh = @fopen($filename, 'w') or die("can't open file");
-@fwrite($fh, '<html lang="en">  <body bgcolor=' . $_POST["colorPick"] . '>');
-@fwrite($fh, ' <header ><img src="../folderD/upload/' . $_FILES["file"]["name"] . '" width="80%" height="300px"></header>   ');
-fwrite($fh, ' <table  width="80%" border =2 cellpadding="2" >');
+@fwrite($fh, '<html lang="en"><head><title>' . $_POST["aLink"] . '</title><link href="style.css" rel="stylesheet"></head>');
+@fwrite($fh, '<body bgcolor=' . $_POST["colorPick"] . '>');
+@fwrite($fh, '<center><header><img class="main" src="upload/' . $_FILES["file"]["name"] . '"></header></center> ');
 
+fwrite($fh, '<center><table>');
 if ((@$_POST["aaTextarea"] != '' || isset($_FILES["filebb"]["tmp_name"])) && @$_POST["delA"] != "no") {
-    fwrite($fh, '<tr><td align="center" <h4>' . $_POST["aaTextarea"] . '</h4></td>');
-    @fwrite($fh, '<td align="center">  <img src = "../folderD/upload/' . $_FILES["filebb"]["name"] . '" > </td></tr>');
+    fwrite($fh, '<tr><td align="center"><h1><p>' . $_POST["aaTextarea"] . '</p></h1></td>');
+    @fwrite($fh, '<td align="center"><img class="minit" src="upload/' . $_FILES["filebb"]["name"] . '"></td></tr>');
 }
-if ((@$_POST["ccTextarea"] != '' || isset($_FILES["filedd"]["tmp_name"])) && @$_POST["delB"] != "no") {
-    fwrite($fh, '<tr><td align="center" <h4>' . $_POST["aaTextarea"] . '</h4></td>');
-    @fwrite($fh, '<td align="center">  <img src = "../folderD/upload/' . $_FILES["filedd"]["name"] . '" > </td></tr>');
+if ((@$_POST["bbtextarea"] != '' || isset($_FILES["filedd"]["tmp_name"])) && @$_POST["delB"] != "no") {
+    fwrite($fh, '<tr><td align="center"><h1><p>' . $_POST["bbtextarea"] . '</p></h1></td>');
+    @fwrite($fh, '<td align="center"><img class="minit" src="upload/' . $_FILES["filedd"]["name"] . '"></td></tr>');
 }
 if ((@$_POST["eeTextarea"] != '' || isset($_FILES["fileff"]["tmp_name"])) && @$_POST["delC"] != "no") {
-    fwrite($fh, '<tr><td align="center" <h4>' . $_POST["aaTextarea"] . '</h4></td>');
-    @fwrite($fh, '<td align="center">  <img src = "../folderD/upload/' . $_FILES["fileff"]["name"] . '" > </td></tr>');
+    fwrite($fh, '<tr><td align="center"><h1><p>' . $_POST["eeTextarea"] . '</p></h1></td>');
+    @fwrite($fh, '<td align="center"><img class="minit" src="upload/' . $_FILES["fileff"]["name"] . '"></td></tr>');
 }
 if ((@$_POST["ggTextarea"] != '' || isset($_FILES["filehh"]["tmp_name"])) && @$_POST["delD"] != "no") {
-    fwrite($fh, '<tr><td align="center" <h4>' . $_POST["aaTextarea"] . '</h4></td>');
-    @fwrite($fh, '<td align="center">  <img src = "../folderD/upload/' . $_FILES["filehh"]["name"] . '" > </td></tr>');
+    fwrite($fh, '<tr><td align="center"><h1><p>' . $_POST["ggTextarea"] . '</p></h1></td>');
+    @fwrite($fh, '<td align="center"><img class="minit" src="upload/' . $_FILES["filehh"]["name"] . '"></td></tr>');
 }
 if ((@$_POST["jjTextarea"] != '' || isset($_FILES["filekk"]["tmp_name"])) && @$_POST["delE"] != "no") {
-    fwrite($fh, '<tr><td align="center" <h4>' . $_POST["aaTextarea"] . '</h4></td>');
-    @fwrite($fh, '<td align="center">  <img src = "../folderD/upload/' . $_FILES["filekk"]["name"] . '" > </td></tr>');
+    fwrite($fh, '<tr><td align="center"><h1><p>' . $_POST["jjTextarea"] . '</p></h1></td>');
+    @fwrite($fh, '<td align="center"><img class="minit" src="upload/' . $_FILES["filekk"]["name"] . '"></td></tr>');
 }
 fwrite($fh, '</table>');
-fwrite($fh, '<a href="1.html">   Home  </a>');
+fwrite($fh, '<h2><a href="1.html">   Home  </a></h2>');
 
-
-fwrite($fh, '</body></html>');
+fwrite($fh, '</center></body></html>');
 fclose($fh);
 ?>
